5c7f073e53d2ae6036d1343ca6c699a97f295c45
Eduardo Loivos <eduardo_loivos@id.uff.br>
2020-11-25 22:49:32 -0300

    função de geração da matriz de cholesky.

--- cholesky.c
+++ cholesky.c
@@ -7,20 +7,56 @@
 
 int* fatoracaoCholesky (int *vaa , int *vja, int *via) {
 
+
+// caa => vetor de dados, cja => vetor de colunas, cia => vetor de linhas.
 int* caa, cja, cia;
+// recuperar qtd de linhas.
 int lin = sizeof via / sizeof via[0];
+// acumulador para posição das linhas e colunas.
 int atual = 0;
-int i , j;
+//acumulador para calculo dos somatórios.
+int gk
+// iteradores.
+int i, j, k;
+// preencher os ponteiros para o inicio das linhas.
 for (i = 0; i < lin-1; i++) {
   atual += i;
   cia[i] = atual;
-
+// preencher as colunas de valor da matriz triangular inferior.
   for (j = 0; j <= i; j++) {
     cja[atual+j] = j;
   }
+// gerar cada valor da matriz c.
 for (i = 0; i < lin-1; i++) {
   for (j = 0; j <= i; j++) {
-    caa[i+j]= 0;
+// preenchimento da diagonal principal.
+    if (i==j) {
+// preenchimento do elemento (0,0).
+      if (i==0) {
+        caa[i+j]= (int) sqrt (vaa[i+j]);
+      }
+// preenchimento dos demais valores da diagonal.
+      else{
+        for (k = 0; k < i; k++) {
+          gk += pow(vaa[i+k],2);
+        }
+        caa[i+j]= (int) sqrt (vaa[] - gk );
+        gk=0;
+      }
+    }else{
+// preenchimento dos valores da coluna (1,j)
+      if (i==0) {
+        caa[i+j] = vaa[i]/caa[0];
+      }
+// preenchimento dos demais valores.
+      else{
+        for (k = 0; k < j; k++) {
+          gk += caa[i+k]*caa[i+j];
+        }
+        caa[i+j] = (vaa[i+j] - gk) / caa[i];
+        gk = 0;
+      }      
+    }
   }
 }
 }
