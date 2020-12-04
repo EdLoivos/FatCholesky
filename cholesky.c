84ba18f339306e4902891b237701fee8326ef7f8
EdLoivos <eduardo_loivos@id.uff.br>
2020-12-04 17:19:12 -0300

    entrada por arquivo

--- cholesky.c
+++ cholesky.c
@@ -1,75 +1,169 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-//[a11,a21,a22,a31,a32,33]
-//[0,0,1,0,1,2]
-//[0,1,3]
+#define HEIGHT 4
+#define WIDTH 5
 
-int fatoracaoCholesky (int *vaa , int *vja, int *via) {
+float **a;
+float **g;
+float **mat;
+
+int fatoracaoCholesky (float **a, int rows, int lb) {
+	//acumulador para calculo dos somatórios.
+	float gk;
+	// iteradores.
+	int i, j, k;
+
+	/*for (i = 0; i < rows; i++){
+		for (j = 0; j <= lb; j++){
+			g[i][j] = 0;
+		}
+	}*/
+
+	//calcula g0,0
+
+	a[0][lb] = (float) sqrt(a[0][lb]);
+
+	//calcula termos das primeiras lb linhas (completa a primeira coluna da matriz original)
+	for (i = 1; i <= lb; i++){
+		for (j = lb-i; j <= lb; j++){
+			if (i+j == lb){//calcula valores da primeira coluna da matriz original
+				a[i][j] = a[i][j]/a[0][lb];
+				a[j+i-lb][2*lb-j] = a[i][j];
+			}
+			else if (j != lb) {//calcula g[i][j]
+				gk = 0;
+				for (k = 0; k < j; k++){
+					gk += a[i][k]*a[j+i-lb][k-j+lb];
+				}
+				a[i][j] = (a[i][j] - gk)/a[j+i-lb][lb];
+				a[j+i-lb][2*lb-j] = a[i][j];
+			}
+			else {//calcula valores da diagonal (g[i][i])
+				gk = 0;
+				for (k = lb-i; k < lb; k++){
+					gk += a[i][k]*a[i][k];
+				}
+				a[i][lb] = (float) sqrt (a[i][lb] - gk);
+			}
+		}
+	}
+
+	for (i = lb+1; i < rows; i++){
+		for (j = 0; j <= lb; j++){
+			if (j != lb) {//calcula g[i][j]
+				gk = 0;
+				for (k = 0; k < j; k++){
+					gk += a[i][k]*a[j+i-lb][k-j+lb];
+				}
+				a[i][j] = (a[i][j] - gk)/a[j+i-lb][lb];
+				a[j+i-lb][2*lb-j] = a[i][j];
+			} else {//calcula valores da diagonal (g[i][i])
+				gk = 0;
+				for (k = 0; k < j; k++){
+					gk += a[i][k]*a[i][k];
+				}
+				a[i][lb] = (float) sqrt (a[i][lb] - gk);
+			}
+		}
+	}
+	return 0;
+}
+
+int resolucaoSistema (float** a, int rows, int lb, float* b, float* x){
+
+	int i, j = 0;
+	float aj = 0;
+	float y[rows];
+
+	for (i = 0; i < rows; i++){
+		x[i] = 0;
+		y[i] = 0;
+	}
+
+	for (i = 0; i < rows; i++){
+		aj = 0;
+		for (j = 0; j < lb; j++){
+			if(i-lb+j >= 0){
+				aj += a[i][j]*y[i-lb+j];
+			}
+		}
+		y[i] = (b[i] - aj)/a[i][lb];
+	}
+
+	for (i = rows-1; i >= 0; i--){
+		aj = 0;
+		for (j = lb; j > 0; j--){
+			if(i+j < rows){
+				aj += a[i][j+lb]*x[i+j];
+			}
+		}
+		x[i] = (y[i] - aj)/a[i][lb];
+	}
+
+	return 0;
+}
+
+int main (int argc, char**argv){
+	char* inputF = argv[1];
+	int lin, col;
+
+	FILE *ent  = fopen(argv[1], "r");
+
+	fscanf(ent, "%d", &lin);
+	fscanf(ent, "%d", &col);
+
+	float **a;
+	float *x = (float*) malloc (sizeof (float) * lin);
+	int i, j;
 
 
-// caa => vetor de dados, cja => vetor de colunas, cia => vetor de linhas.
-int* caa, cja, cia;
-// recuperar qtd de linhas.
-int lin = sizeof via / sizeof via[0];
-// acumulador para posição das linhas e colunas.
-int atual = 0;
-//acumulador para calculo dos somatórios.
-int gk;
-// iteradores.
-int i, j, k;
-// preencher os ponteiros para o inicio das linhas.
-for (i = 0; i < lin-1; i++) {
-  atual += i;
-  cia[i] = atual;
-// preencher as colunas de valor da matriz triangular inferior.
-  for (j = 0; j <= i; j++) {
-    cja[atual+j] = j;
-  }
-}
-// gerar cada valor da matriz c.
-for (i = 0; i < lin-1; i++) {
-  for (j = 0; j <= i; j++) {
-// preenchimento da diagonal principal.
-    if (i==j) {
-// preenchimento do elemento (0,0).
-      if (i==0) {
-        caa[cia[i]+j]= (int) sqrt (vaa[via[i]+j]);
-      }
-// preenchimento dos demais valores da diagonal.
-      else{
-        while (k<i && vji[cia[i]+k]>vji[cia[i]+k-1]) {
-          gk += pow(vaa[via[i]+k],2);
-        }k=0;
-        while (k<=j) {
-          if (vja[cia[i]+k]==j) {
-            caa[cia[i]+j]= (int) sqrt (vaa[via[i]+k] - gk );
-          }else{
-            caa[cia[i]+j]= (int) sqrt (0 - gk );}
-        }
-        gk=0;
-      }
-    }else{
-// preenchimento dos valores da coluna (1,j)
-      if (i==0) {
-        caa[cia[i]+j] = vaa[i]/caa[0];
-      }
-// preenchimento dos demais valores.
-      else{
-        while (k<i && vji[cia[i]+k]>vji[cia[i]+k-1]) {
-          gk += caa[cia[i]+k]*caa[k+cia[j]];
-        }
-        while (k<=j) {
-          if (vja[cia[i]+k]==j) {
-            caa[cia[i]+j] = (vaa[via[i]+k] - gk) / caa[cia[j]+j];
-          }else{
-            caa[cia[i]+j] = (0 - gk) / caa[cia[j]+j];
-        }
-        gk = 0;
-      }
-    }
-  }
-}
-}
-return caa;
+	/*float g[HEIGHT][WIDTH] = {	{0,0,4,1,-1},
+						{0,1,4,1,-1},
+						{-1,1,4,1,-1},
+						{-1,1,4,1,-1},
+						{-1,1,4,1,-1},
+						{-1,1,4,1,-1},
+						{-1,1,4,1,-1},
+						{-1,1,4,1,-1},
+						{-1,1,4,1,0},
+						{-1,1,4,0,0}
+	};*/
+
+	//float g[HEIGHT][WIDTH] = {{0,0,4},{0,12,37},{-16,-43,98}};
+	//float g[HEIGHT][WIDTH] = {{0,0,5,-4,1},{0,-4,6,-4,1},{1,-4,6,-4,0},{1,-4,5,0,0}};
+
+	//float b[HEIGHT] = {4,5,4,4,4,4,4,4,5,4};
+	//float b[HEIGHT] = {1,2,3};
+	//float b[HEIGHT] = {0,1,0,0};
+
+	a =(float**) malloc(sizeof (float *) * lin);
+
+	for (i = 0; i < lin; i++){
+		a[i] =(float*) malloc (sizeof (float) * col);
+	}
+
+	for (i = 0; i < lin; i++){
+		for (j = 0; j < col; j++){
+			fscanf(ent, "%d", a[i][j]) ;
+		}
+	}
+	for (i = 0; i< lin;i++){
+		fscanf(ent, "%d", x[i]) ;
+	}
+
+	fatoracaoCholesky(a, lin, (col-1)/2);
+	resolucaoSistema(a, lin, (col-1)/2, b, x);
+
+	for (i = 0; i < lin; i++){
+		printf("%f\n", x[i]);
+	}
+
+	for (i = 0; i < lin; i++){
+		free(a[i]);
+	}
+	free(a);
+	free(x);
+
+	return 0;
 }
